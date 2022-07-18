39182dd1f80abb405076175f31d852e5eaf6eccd
thiago <thiago.bsb@hotmail.com>
2022-07-18 09:26:42 -0300

    padrao de sidebar responsivel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+    constructor(private breakpointObserver: BreakpointObserver){}
+
   /* title = 'calangeek-portifolio'; */
   opened= false
 }
